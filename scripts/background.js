4220aee216e658689eeded5e24c01fd2a6049246
Asif-Arafat-Rafat <asif.arafat.rafat@gmail.com>
2025-01-25 15:59:13 +0600

    Scripting Function Working

--- scripts/background.js
+++ scripts/background.js
@@ -3,5 +3,21 @@ chrome.runtime.onInstalled.addListener(()=>{
     console.log("Extension installed!");
 });
 chrome.tabs.onUpdated.addListener((tabid, changeInfo, tab) => {
-    console.log("Tab updated:", tabid, changeInfo, tab);
+    if(changeInfo.status==="complete"){
+        console.log("Loading complete for tab:",tab.url);
+        chrome.scripting.executeScript({
+            target: {tabId: tabid},
+            function: Inlogin
+        }).then((result)=>{
+            console.log("Result:",result[0].result);
+        });
+        
+    }
 });
+
+function Inlogin(){
+    console.log("Inlogin Ran");
+    const login= document.querySelectorAll('input[type="password"]');
+    console.log("Password Fields Found here:",login.length);
+    return login.length ;
+}
