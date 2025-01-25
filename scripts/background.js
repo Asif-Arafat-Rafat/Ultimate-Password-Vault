06901eed0108dd0b755f74a2c127943b5a6ea1ba
Asif-Arafat-Rafat <asif.arafat.rafat@gmail.com>
2025-01-25 15:10:09 +0600

    Tab Update Action Working

--- scripts/background.js
+++ scripts/background.js
@@ -1,4 +1,7 @@
 console.log("Background script loaded!");
 chrome.runtime.onInstalled.addListener(()=>{
-    chrome.tabs.create({ url: "https://www.google.com/" });
-})
+    console.log("Extension installed!");
+});
+chrome.tabs.onUpdated.addListener((tabid, changeInfo, tab) => {
+    console.log("Tab updated:", tabid, changeInfo, tab);
+});
